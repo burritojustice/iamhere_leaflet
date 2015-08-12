b0beedc497dd57db70a8b3cd24e9152a542807f0
Elizabeth <elizabeth.williams@mapzen.com>
2015-08-12 17:49:28 -0400

    commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var map = L.map('map').setView([51.505, -0.09], 17);
+var layer = Tangram.leafletLayer({ scene: 'https://raw.githubusercontent.com/tangrams/simple-demo/gh-pages/scene.yaml' });
+layer.addTo(map);
+
+var latlng = document.getElementById('latlng');
+// $('#latlng')
+
+var marker = L.marker([51.505, -0.09], {
+    draggable: false
+}).addTo(map);
+
+//map.locate({setView: true, maxZoom: 16});
+
+//updateLatLng();
+
+map.on('move', recenter);
+function recenter(){
+  marker.setLatLng(map.getCenter());
+}
+
+map.on('dragend', updateLatLng);
+
+var geojson;
+
+function updateLatLng(){
+    var m = marker.getLatLng();
+    var latParam = m.lat.toFixed(6);
+    var lngParam = m.lng.toFixed(6);
+    latlng.innerHTML = latParam + ', ' + lngParam;
+    $.getJSON('https://54.148.56.3/?latitude='+latParam+'&longitude='+lngParam, function(data){
+      geojson = data;
+      $('#stuff').text(geojson.features[4].properties['wof:name']);
+    })
+    //$('#stuff').text(geojson.features[0].properties['wof:name']);
+}
+
+document.getElementById('locate').addEventListener('click', function test(){
+    map.locate({setView: true, maxZoom: 17});
+    map.on('locationfound',updateLatLng);
+});
